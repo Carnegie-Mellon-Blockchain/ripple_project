ddc56eff50052bdf2e548d1d374032f3e7d26181
Janice He <chihyan50he@gmail.com>
2025-02-08 15:54:35 -0500

    frontend list videos, quizzes

--- frontend/src/VideoPage.js
+++ frontend/src/VideoPage.js
@@ -1,14 +1,29 @@
 import { useParams } from "react-router-dom";
 import Header from './Header';
+import videos from "./videos.json";
+import Quiz from "./Quiz.js";
 
 function VideoPage() {
   const { videoId } = useParams(); // Extract the videoId from URL
+
+  // Find the specific video from JSON
+  const video = videos.find((v) => v.id === videoId);
+
+  // If video not found, show an error message
+  if (!video) {
+    return <h2 style={{ textAlign: "center", color: "red" }}>Video not found</h2>;
+  }
 
   return (
     <div style={styles.appContainer}>
       <Header />
       <div style={styles.content}>
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/jfKfPfyJRdk?si=eVFTODuklYF5JcG2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+		<h2>{video.title}</h2>
+		<p>{video.description}</p>
+		<iframe width="560" height="315" src={`https://www.youtube.com/embed/${video.id}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+		<Quiz quizzes={video.quiz} videoId={video.id} />
+
 	  </div>
 	</div>
   );
