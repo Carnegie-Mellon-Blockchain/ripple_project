c063244cbbae329ce10a8094b895bf9033c7a255
dunsinf <dunsin.fadojutimi@autodesk.com>
2025-02-08 02:32:15 -0500

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import ConnectWallet from './ConnectWallet';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ConnectWallet />
+      {/* Other components will go here */}
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/VideoPlayer.js
@@ -0,0 +1,21 @@
+// src/VideoPlayer.js
+import React from 'react';
+import YouTube from 'react-youtube';
+
+const VideoPlayer = () => {
+    const videoId = 'OyCVgveTcX4';
+    const startTime = 5689;
+
+  const opts = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      autoplay: 0,
+      start: startTime,
+    },
+  };
+
+  return <YouTube videoId={videoId} opts={opts} />;
+};
+
+export default VideoPlayer;
